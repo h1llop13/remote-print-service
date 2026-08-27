75a0e52a343897b985220141c8297cc470a698d1
h1llop13 <h1llapple13@gmail.com>
2026-08-27 18:25:00 +0500

    complete MVP 1 error handling

--- src/main/java/com/remoteprint/exception/GlobalExceptionHandler.java
+++ src/main/java/com/remoteprint/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.Map;
 
@@ -35,6 +36,20 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxUploadSizeExceededException(
+            MaxUploadSizeExceededException exception
+    ) {
+
+        Map<String, String> response = Map.of(
+                "error", "File exceeds maximum allowed size"
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.PAYLOAD_TOO_LARGE)
                 .body(response);
     }
 }
